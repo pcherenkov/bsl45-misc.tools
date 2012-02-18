a302df9ef5b6f9a7c799212f54da1e8a3e7e98ac
Pavel Cherenkov <pcherenkov@gmail.com>
2012-02-18 18:32:10 +0300

    xvdat header added

---
+++ fdwatch/xvdat.h
@@ -0,0 +1,59 @@
+/* @(#) extensible vector-based data structures */
+
+#ifndef XVDATH_20120218
+#define XVDATH_20120218
+
+#include <sys/types.h>
+
+/* extensible C array */
+struct xvec {
+    void    *data;
+    size_t  len,
+            max_len;
+};
+
+
+/* xvec-based list */
+struct xvlst {
+    void    *data;
+    size_t  len,
+            max_len;
+
+    char    *taken;
+    size_t  border; /* index of last element ever used */
+    ssize_t avail;  /* index of min available element */
+};
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+int
+xvec_reserve(struct xvec *v, size_t new_max, size_t blksz, size_t growblk);
+
+int
+xvec_expand(struct xvec *v, size_t plus, size_t blksz, size_t growblk);
+
+int
+xvlst_reserve(struct xvlst *l, size_t new_max, size_t blksz, size_t growblk);
+
+int
+xvlst_expand(struct xvlst *l, size_t plus, size_t blksz, size_t growblk);
+
+ssize_t
+xvlist_add(struct xvlst *l);
+
+int
+xvlist_del(struct xvlst *l, size_t index);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* XVDATH_20120218 */
+
+/* __EOF__ */
+
