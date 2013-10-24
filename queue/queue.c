7c1d83859724fbda6fa0ca6732e488450b199bf4
Pavel Cherenkov <pcherenkov@gmail.com>
2013-10-24 12:38:14 +0400

    queue: added to.

--- queue/queue.c
+++ queue/queue.c
@@ -4,16 +4,63 @@ struct pqueue {
 	void		**data;
 	size_t		max_count;
 	size_t		count;
-	size_t		head, tail;
-	size_t		vhead, vtail;
+	ssize_t		head, tail;
 };
 
 
+/* Create a new pointer queue. */
 long
-pqueue_init(size_t max_count)
+pqueue_create(size_t max_count)
 {
-	/* TODO: */
+	struct pqueue *pq = calloc(1, sizeof(*pq));
+
+	if (NULL == pq) return -1;
+
+	pq->data = calloc(max_count, sizeof(pq->data[0]));
+	if (NULL == pq->data) {
+		free(pq);
+		errno = ENOMEM;
+		return -1;
+	}
+
+	pq->max_count = max_count;
+	pq->count = 0;
+	pq->head = pq->tail = -1;
+
+	return (long)pq;
 }
+
+
+/* Release resources allocated for the queue. */
+void
+pqueue_free(long q)
+{
+	struct pqueue *pq = (struct pqueue*)q;
+
+	if (0 == q) return;
+
+	if (pq->data)
+		free(pq->data);
+	free(pq);
+}
+
+
+/* Return non-zero if queue is empty. */
+int
+pqueue_empty(long q)
+{
+	struct pqueue *pq = (struct pqueue*)q;
+	return 0 == pq->count;
+}
+
+
+/* Get the next item, return -1. */
+void*
+pqueue_next(long q)
+{
+/* TODO: */
+}
+
 
 
 /* __EOF__ */
