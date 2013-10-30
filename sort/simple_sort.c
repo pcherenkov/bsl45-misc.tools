256555958e08a488f60f7ff0753c3c0e87755d16
Pavel Cherenkov <pcherenkov@gmail.com>
2013-10-30 09:47:31 +0400

    sort: insertion sort added.

--- sort/simple_sort.c
+++ sort/simple_sort.c
@@ -13,23 +13,40 @@ swap(long *v1, long *v2)
 }
 
 
-/* Selection sort */
+/* Selection sort. */
 void
 selsort(long *val, size_t num)
 {
 	size_t i = 0, j = 0, min = 0;
 
-	if (num < 2)
-		return;
+	if (num < 2) return;
 
-	for (i = 1; i < num; ++i) {
-		min = i - 1;
-		for (j = i; j < num; ++j) {
+	for (i = 0; i < num; ++i) {
+		min = i;
+		for (j = i + 1; j < num; ++j) {
 			if (val[min] < val[j])
 				min = j;
 		}
 		if (min != i -1)
 			swap(&val[min], &val[i-1]);
+	}
+}
+
+
+/* Insertion sort. */
+void
+insort(long *val, size_t num)
+{
+	ssize_t i = 0, j = 0;
+	long v = 0;
+
+	if (num < 2) return;
+
+	for (i = 1; i < (ssize_t)num; ++i) {
+		v = val[i];
+		for (j = i - 1; j >= 0 && val[j] > v; --j)
+			val[j + 1] = val[j];
+		val[j] = v;
 	}
 }
 
